e5620e6b2fed34e4bff99a9c6d928a80b8e265e7
karenbarseghyan90@gmail.com <karenbarseghyan90@gmail.com>
2022-07-15 12:08:38 +0400

    Fetching the news from json db

--- src/pages/News.js
+++ src/pages/News.js
@@ -1,6 +1,18 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 
 const News = () => {
+    const [news, setNews] = useState(null);
+
+    useEffect(()=>{
+        fetch('http://localhost:8000/news')
+        .then(res => {
+            return res.json()
+            .then(data => {
+                setNews(data)
+            })
+        })
+    }, [])
+
     return (
         <div>
             News
