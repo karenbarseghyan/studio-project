6aedde3e4d4f601090e5a5b286ee86d694ee8f98
karenbarseghyan90@gmail.com <karenbarseghyan90@gmail.com>
2022-07-15 23:07:37 +0400

    Displaying news Inner-page details

--- src/components/news/NewsDetails.js
+++ src/components/news/NewsDetails.js
@@ -1,11 +1,21 @@
 import React from 'react'
 import { useParams } from 'react-router-dom'
+import { useSelector } from 'react-redux'
 
 const NewsDetails = () => {
     const {id} = useParams()
+    const news = useSelector((state)=> state.news)
+    console.log("---",id);
     return (
         <div>
             DETAILS OF NEWS {id}
+            {news.filter((news)=> news.id == id).map((news)=> (
+                <div  key={news.id}> 
+                    <h3>{news.title}</h3>
+                    <p>{news.description}</p>
+                    <img src={news.image} alt="news" height="200px" />
+                </div>
+            ))}
         </div>
     )
 }
