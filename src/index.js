eb56b5dce9063aac1b628d8a9b13c2c3aa894d3a
karenbarseghyan90@gmail.com <karenbarseghyan90@gmail.com>
2022-07-15 17:02:25 +0400

    Connection of Redux

--- src/index.js
+++ src/index.js
@@ -2,13 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
+import { Provider} from 'react-redux'
+import { createStore, applyMiddleware, compose } from 'redux';
+import thunk from 'redux-thunk';
+import reducers from './reducers'
 
+const store = createStore(reducers, compose(applyMiddleware(thunk)))
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <BrowserRouter >
-      <App />
-    </BrowserRouter>
-  </React.StrictMode>
+  <Provider store={store}>
+      <BrowserRouter >
+        <App />
+      </BrowserRouter>
+  </Provider>
 );
 
