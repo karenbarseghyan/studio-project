016df4b5fe609f730ad0b94fced23e7c06fca187
karenbarseghyan90@gmail.com <karenbarseghyan90@gmail.com>
2022-07-15 14:47:18 +0400

    News Search-by-Title funcationality

--- src/components/news/NewsList.js
+++ src/components/news/NewsList.js
@@ -1,12 +1,12 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 
-const NewsList = ({news, title}) => {
+const NewsList = ({news, search, title}) => {
     console.log(news)
     return (
         <div>
             <h1>{title}</h1>
-            {news.map((news)=> (
+            {news.filter((news)=>news.title.toLowerCase().includes(search)).map((news)=> (
                 <div  key={news.id}> 
                 <Link to={`/news/${news.id}`}>
                     <h3>{news.title}</h3>
--- src/pages/News.js
+++ src/pages/News.js
@@ -1,12 +1,14 @@
+import { useState } from 'react'
 import NewsList from '../components/news/NewsList'
 import useFetch from '../useFetch'
 
 const News = () => {
     const { data: news } = useFetch('http://localhost:8000/news')
-
+    const [search, setSearch] = useState("")
     return (
         <div>
-            {news && <NewsList news={news} title="Latest News" />}
+            <input type="text" placeholder="Search..." onChange={e=>setSearch(e.target.value)}/>
+            {news && <NewsList news={news} search={search} title="Latest News" />}
         </div>
     )
 }
