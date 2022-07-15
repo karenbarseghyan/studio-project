9d2598ec513bb2eebc285fd796ce824651dac815
karenbarseghyan90@gmail.com <karenbarseghyan90@gmail.com>
2022-07-15 23:10:24 +0400

    Creating actionType constants for Redux

--- src/actions/news.js
+++ src/actions/news.js
@@ -1,9 +1,10 @@
 import * as api from '../api'
+import { FETCH_ALL } from '../constants/actionTypes'
 
 export const getNews = () => async(dispatch) => {
     try {
         const { data } = await api.fetchNews() ;
-        dispatch({type: 'FETCH_ALL', payload: data})
+        dispatch({type: FETCH_ALL, payload: data})
     } catch(e) { 
         console.log(e)
     }
--- src/reducers/news.js
+++ src/reducers/news.js
@@ -1,6 +1,8 @@
+import { FETCH_ALL } from "../constants/actionTypes";
+
 export default (news=[], action) => {
     switch(action.type){
-        case 'FETCH_ALL' : 
+        case FETCH_ALL : 
             return action.payload;
         default:
             return news;
