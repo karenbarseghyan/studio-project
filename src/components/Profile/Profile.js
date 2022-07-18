b39d31ffb4e42f76a2df376ff147e5444d98539f
karenbarseghyan90@gmail.com <karenbarseghyan90@gmail.com>
2022-07-18 09:18:54 +0400

    Profile page design

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -1,9 +1,11 @@
 import React from 'react'
+import './profile.css'
 
 const Profile = () => {
     return (
-        <div>
-            Profile
+        <div className='profileContainer'>
+           <h1>Profile</h1> 
+           <h2>You are authorized user</h2>
         </div>
     )
 }
