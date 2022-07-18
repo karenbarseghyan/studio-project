fd018ee7535668dfef4908a50efbbcb36bed6790
karenbarseghyan90@gmail.com <karenbarseghyan90@gmail.com>
2022-07-18 09:20:31 +0400

    Home page Video set

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -3,7 +3,11 @@ import React from 'react'
 const Home = () => {
     return (
         <div>
-            Home
+            <video loop={true} muted width="100%" autoPlay={true}>
+                <source src="https://www.studio-one.am/resources/studio/video/video.webm" type="video/webm"/>
+                <source src="https://www.studio-one.am/resources/studio/video/video.mp4" type="video/mp4"/>                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                     
+                <source src="https://www.studio-one.am/resources/studio/video/video.ogg" type="video/ogg"/>
+            </video>
         </div>
     )
 }
