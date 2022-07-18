e3c2d1f4b97c14ce3857064a504182b6505a05a6
karenbarseghyan90@gmail.com <karenbarseghyan90@gmail.com>
2022-07-18 09:17:00 +0400

    News Item page destructuring and design

---
+++ src/components/newsItem/NewsItem.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import { useParams } from 'react-router-dom'
+import { useSelector } from 'react-redux'
+import { Link } from 'react-router-dom'
+
+
+import './newsItem.css'
+
+const NewsItem = ({image, title, description, id}) => {
+    return (
+        <div>                
+            <div  className='newsItemWrapper' key={id}> 
+                <Link to={`/news/${id}`}>
+                    <img className='newItemImage' src={image} alt={title}/>
+                    <h3>{title}</h3>
+                    <p>{description}</p>
+                </Link>
+            </div>
+        </div>
+    )
+}
+
+export default NewsItem
